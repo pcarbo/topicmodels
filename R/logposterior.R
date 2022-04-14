8fa2df08f573e9efba373e27ebf15a33b09da164
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-04-14 07:25:36 -0500

    Implemented rcost C routine for log-posterior calculations.

--- R/logposterior.R
+++ R/logposterior.R
@@ -11,18 +11,30 @@ logposterior_multinom <- function (x, F, L, alpha, delta, e = 1e-8) {
 
 # Computes loss function equal to negative log-likelihood, ignoring
 # terms that do not depend on F and L.
-cost <- function (X, A, B, e = 1e-8, version = c("R","cpp")) {
+#
+#   # In this example y1 and y2 should be the same.
+#   set.seed(1)
+#   n <- 8
+#   m <- 12
+#   k <- 4
+#   X <- matrix(rpois(n*m,4),n,m)
+#   A <- matrix(abs(rnorm(n*k)),n,k)
+#   B <- matrix(abs(rnorm(k*m)),k,m)
+#   y1 <- cost(X,A,B,version = "r")
+#   y2 <- cost(as(X,"dgCMatrix"),A,B,version = "cpp")
+#
+cost <- function (X, A, B, e = 1e-8, version = c("cpp","r")) {
   version <- match.arg(version)
-  if (is.matrix(X) | version == "R")
+  if (is.matrix(X) | version == "R") {
     y <- -rowSums(X*log(A %*% B + e))
-  else {
+  } else {
     n <- nrow(X)
     m <- ncol(X)
     k <- ncol(A)
     x <- summary(X)
     y <- .Call(C_rcost,as.integer(n),as.integer(m),as.integer(k),
-               as.integer(x$i),as.integer(x$j),as.numeric(x$x),
-               as.numeric(e))
+               as.integer(x$i - 1),as.integer(x$j - 1),as.numeric(x$x),
+               as.numeric(t(A)),as.numeric(B),as.numeric(e))
   }
   return(y)
 }
--- src/rcost.c
+++ src/rcost.c
@@ -3,15 +3,23 @@
 
 // Implements core calculations for cost(X,A,B,e,version = "R") when X
 // is a sparse matrix.
-SEXP rcost (SEXP np, SEXP mp, SEXP kp, SEXP ip, SEXP jp, SEXP xp, SEXP ep) {
+SEXP rcost (SEXP np, SEXP mp, SEXP kp, SEXP ip, SEXP jp, SEXP xp, 
+	    SEXP ap, SEXP bp, SEXP ep) {
   int     n;
   int     m;
   int     k;
+  int     t;
+  int     N;
+  int     K;
+  int     it;
+  int     jt;
   int*    i;
   int*    j;
   double  ab;
   double  e;
   double* x;
+  double* a;
+  double* b;
   double* y;
   SEXP    yp;
 
@@ -22,17 +30,21 @@ SEXP rcost (SEXP np, SEXP mp, SEXP kp, SEXP ip, SEXP jp, SEXP xp, SEXP ep) {
   ip = PROTECT(coerceVector(ip,INTSXP));
   jp = PROTECT(coerceVector(jp,INTSXP));
   xp = PROTECT(coerceVector(xp,REALSXP));
+  ap = PROTECT(coerceVector(ap,REALSXP));
+  bp = PROTECT(coerceVector(bp,REALSXP));
   ep = PROTECT(coerceVector(ep,REALSXP));
   n  = *INTEGER(np);
   m  = *INTEGER(mp);
-  k  = *INTEGER(kp);
+  K  = *INTEGER(kp);
   i  = INTEGER(ip);
   j  = INTEGER(jp);
   x  = REAL(xp);
+  a  = REAL(ap);
+  b  = REAL(bp);
   e  = *REAL(ep);
 
   // Initialize the output.
-  yp = PROTECT(allocVector(REALSXP,1));
+  yp = PROTECT(allocVector(REALSXP,n));
   y  = REAL(yp);
 
   // This is equivalent to the following R code:
@@ -43,39 +55,18 @@ SEXP rcost (SEXP np, SEXP mp, SEXP kp, SEXP ip, SEXP jp, SEXP xp, SEXP ep) {
   // 
   //   ab = sum(A[i,] * B[,j])
   //
-  n = length(xp);
-  *y = 0;
-  for (int i = 0; i < n; i++) {
-    // ab = A * B.col(j);
-    ab = 1;
-    *y -= x[i] * log(ab + e);
+  N = length(xp);
+  for (t = 0; t < n; t++)
+    y[t] = 0;
+  for (t = 0; t < N; t++) {
+    it = i[t];
+    jt = j[t];
+    ab = 0;
+    for (k = 0; k < K; k++)
+      ab += a[it*K + k] * b[jt*K + k];
+    y[it] -= x[t] * log(ab + e);
   }
 
-  UNPROTECT(8);
+  UNPROTECT(10);
   return yp;
-}
-
-SEXP convolve2 (SEXP a, SEXP b) {
-  int na, nb, nab;
-  double *xa, *xb, *xab;
-  SEXP ab;
-
-  a = PROTECT(coerceVector(a,REALSXP));
-  b = PROTECT(coerceVector(b,REALSXP));
-  na = length(a); 
-  nb = length(b); 
-  nab = na + nb - 1;
-  ab = PROTECT(allocVector(REALSXP,nab));
-  xa = REAL(a); 
-  xb = REAL(b); 
-  xab = REAL(ab);
-
-  for (int i = 0; i < nab; i++) 
-    xab[i] = 0.0;
-  for (int i = 0; i < na; i++)
-    for (int j = 0; j < nb; j++) 
-      xab[i + j] += xa[i] * xb[j];
-
-  UNPROTECT(3);
-  return ab;
 }
